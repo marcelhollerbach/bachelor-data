6de206cf115b26cd07115a44b281b4587da6b9b6
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-08-17 11:40:28 +0200

    new data

--- safed-bitwidth.r
+++ safed-bitwidth.r
@@ -1,12 +1,1 @@
-data = NULL;
-
-for(project in c("zlib", "libjpeg", "libpng", "libgif", "libtiff")) {
-  csv = read.csv(paste(project, '/data.csv', sep = ""))
-  colnames(csv) <- c("function", "pre", "after")
-  pd = data.frame(project, mean(csv$pre), mean(csv$pre) - mean(csv$after))
-  colnames(pd) <- c("lib", "average_bitwidth", "safed_bitwidth")
-  data = rbind(data, pd)
-} 
-write.csv(data, 'result.csv')
-plot(data)
-View(data)
+zlib = read.csv('bla')
